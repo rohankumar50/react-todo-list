fc88c460357cfee3acaa28ce3c8e71ede5df7c49
rohankumarchs50 <rohankumarchs@gmail.com>
2020-10-29 20:47:51 +0530

    todo list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [ input, setInput ] = useState('');
+	const [ items, setitems ] = useState([]);
+
+	const listOfItems = () => {
+		setitems((oldItem) => {
+			return [ ...oldItem, input ];
+		});
+		setInput('');
+	};
+
+	const deleteItem = (index) => {
+		setitems((oldItem) => {
+			return oldItem.filter((arrEle, i) => {
+				return i !== index;
+			});
+		});
+	};
+
+	return (
+		<div className="main_div">
+			<div className="center_div">
+				<br />
+				<h1>Todo list</h1>
+				<br />
+				<input
+					onChange={(e) => setInput(e.target.value)}
+					value={input}
+					type="text"
+					placeholder="Add an items"
+				/>
+				<button onClick={listOfItems}>+</button>
+				<ol>
+					{items.map((itemlist, index) => {
+						return (
+							<div className="todo_style" key={index}>
+								<i className="fa fa-times" aria-hidden="true" onClick={() => deleteItem(index)} />
+								<li>{itemlist}</li>
+							</div>
+						);
+					})}
+				</ol>
+			</div>
+		</div>
+	);
+};
 
 export default App;
